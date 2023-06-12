a4963615fb95f9b18bac065738911c591b176ce3
douglasdl <douglas_san@hotmail.com>
2023-06-12 12:40:02 -0300

    Updated

--- 03-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
@@ -17,11 +17,15 @@ type NewTransactionsFormInputs = z.infer<typeof newTransactionFormSchema>;
 export function NewTransactionModal() {
 
   const { 
+    control,
     register, 
     handleSubmit,
     formState: { isSubmitting }
   } = useForm<NewTransactionsFormInputs>({
-    resolver: zodResolver(newTransactionFormSchema)
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: {
+      type: 'income'
+    }
   });
 
   async function handleCreateNewTransaction(data: NewTransactionsFormInputs) {
@@ -60,22 +64,33 @@ export function NewTransactionModal() {
             {...register('category')}
           />
 
-          <TransactionType>
-            <TransactionTypeButton 
-              variant='income'
-              value='income'
-            >
-              <ArrowCircleUp size={24} />
-              Entrada
-            </TransactionTypeButton>
-            <TransactionTypeButton 
-              variant='outcome'
-              value='outcome'
-            >
-              <ArrowCircleDown size={24} />
-              Saída
-            </TransactionTypeButton>
-          </TransactionType>
+          <Controller 
+            control={control}
+            name='type'
+            render={({ field }) => {
+              return (
+                <TransactionType 
+                  onValueChange={field.onChange} 
+                  value={field.value}
+                >
+                  <TransactionTypeButton 
+                    variant='income'
+                    value='income'
+                  >
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+                  <TransactionTypeButton 
+                    variant='outcome'
+                    value='outcome'
+                  >
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
+          />
 
           <button 
             type="submit"
--- 03-dt-money/src/contexts/TransactionsContext.tsx
+++ 03-dt-money/src/contexts/TransactionsContext.tsx
@@ -12,6 +12,7 @@ interface ITransaction {
 
 interface TransactionContextType {
     transactions: ITransaction[];
+    fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -24,18 +25,27 @@ export function TransactionsProvider({children} : TransactionsProviderProps) {
 
     const [transactions, setTransactions] = useState<ITransaction[]>([]);
 
-    async function loadTransactions() {
-        const response = await fetch("http://192.168.10.112:3333/transactions")
+    async function fetchTransactions(query?: string) {
+        const url = new URL("http://192.168.10.112:3333/transactions");
+
+        if(query) {
+            url.searchParams.append('q', query);
+        }
+
+        const response = await fetch(url)
         const data = await response.json()
         setTransactions(data);
     }
 
     useEffect(() => {
-        loadTransactions();
+        fetchTransactions();
     }, []);
 
     return (
-        <TransactionsContext.Provider value={{ transactions }}>
+        <TransactionsContext.Provider value={{ 
+            transactions,
+            fetchTransactions
+        }}>
             {children}
         </TransactionsContext.Provider>
     )
